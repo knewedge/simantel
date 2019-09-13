556c47eb87d41aef1e210170478a997bae264983
Nick Eldred <knewedge@gmail.com>
2019-09-12 23:16:18 -0500

    added contact section styles

--- front-page.php
+++ front-page.php
@@ -62,18 +62,18 @@
 				<h4 class="contact-head">Founding Members</h4>
 				<div class="row">
 					<div class="col-md-6">
-						<h3>HEADSHOT HERE</h3>
+						<strong>HEADSHOT HERE</strong>
 					</div>
 					<div class="col-md-6">
-						<h3>HEADSHOT HERE</h3>
+						<strong>HEADSHOT HERE</strong>
 					</div>
 				</div>
 			</div>
 			<div class="col-md-8">
 				<div class="row">
 					<div class="col-md-4">
-						321 SW Water St<br />
-						Peoria, IL 61602
+						<strong>321 SW Water St<strong><br />
+						<strong>Peoria, IL 61602</strong>
 					</div>
 					<div class="col-md-4">
 						<strong>phone:</strong><br />
@@ -87,7 +87,7 @@
 						<div class="col-md-12">
 						<i class="fab fa-facebook-square"></i>
 						<i class="fab fa-twitter"></i>
-						<i class="fab fa-instagram-square"></i>
+						<i class="fab fa-instagram"></i>
 						</div>
 					</div>
 				</div>

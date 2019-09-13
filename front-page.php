344343c9cb68c3b53d1d2cd69fb476636a7214c9
Nick Eldred <knewedge@gmail.com>
2019-09-12 21:17:02 -0500

    added front page

---
+++ front-page.php
@@ -0,0 +1,35 @@
+<?php
+/*
+* Template Name: Home Page
+* Home page Layout
+*/
+?>
+<?php get_header(); ?>
+<?php do_action('__before_main_wrapper'); ?>
+<section class="featured-slider">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 text-center">
+				<h3>Give</h3>
+			</div>
+			<div class="col-md-3 text-center">
+				<h3>Help</h3>
+			</div>
+			<div class="col-md-3 text-center">
+				<h3>Learn</h3>
+			</div>
+			<div class="col-md-3 text-center">
+				<h3>Create</h3>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="call-to-action">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12"></div>
+		</div>
+	</div>
+</section>
+<?php do_action( '__after_body' ); ?>
+</html>

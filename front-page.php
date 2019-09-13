3b8245ec52060727c830783a0f083f38f2724a33
Nick Eldred <knewedge@gmail.com>
2019-09-12 21:35:20 -0500

    fixed img src on front page

--- front-page.php
+++ front-page.php
@@ -18,7 +18,7 @@
 		</div>
 		<div class="row">
 			<div class="col-md-4">
-				<img class="rounded-circle" src="/wp-content/uploads/2019/09/banner1.jpg" alt="kids loooking throuh fence" />
+				<img class="rounded-circle" src="/simantel/wp-content/uploads/2019/09/banner1.jpg" alt="kids loooking throuh fence" />
 			</div>
 			<div class="col-md-8">
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>

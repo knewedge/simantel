6e69b91073840d229fb6bd6ced4ac6a3e73022fc
Nick Eldred <knewedge@gmail.com>
2019-09-12 23:05:33 -0500

    added contact section

--- front-page.php
+++ front-page.php
@@ -55,6 +55,54 @@
 		</div>
 	</div>
 </section>
+<section class="contact">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<h4 class="contact-head">Founding Members</h4>
+				<div class="row">
+					<div class="col-md-6">
+						<h3>HEADSHOT HERE</h3>
+					</div>
+					<div class="col-md-6">
+						<h3>HEADSHOT HERE</h3>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-8">
+				<div class="row">
+					<div class="col-md-4">
+						321 SW Water St<br />
+						Peoria, IL 61602
+					</div>
+					<div class="col-md-4">
+						<strong>phone:</strong><br />
+						309.674.7747
+					</div>
+					<div class="col-md-4">
+						<strong>email:</strong><br />
+						info@simantel.com
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+						<i class="fab fa-facebook-square"></i>
+						<i class="fab fa-twitter"></i>
+						<i class="fab fa-instagram-square"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row featured-copy">
+			<div class="col-md-4">
+				<img class="image-circle" src="/simantel/wp-content/uploads/2019/09/banner1-square.jpg" alt="kids loooking throuh fence" />
+			</div>
+			<div class="col-md-8">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+		</div>
+	</div>
+</section>
 <?php do_action( '__after_body' ); ?>
 <?php get_footer(); ?>
 </html>

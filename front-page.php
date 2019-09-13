d5e5ccfe3c0ff33c6e56e10ee9db13ff8e17d14a
Nick Eldred <knewedge@gmail.com>
2019-09-12 21:32:19 -0500

    added feature slider section to front page

--- front-page.php
+++ front-page.php
@@ -16,6 +16,14 @@
 				<h3>Create</h3>
 			</div>
 		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<img class="rounded-circle" src="/wp-content/uploads/2019/09/banner1.jpg" alt="kids loooking throuh fence" />
+			</div>
+			<div class="col-md-8">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+		</div>
 	</div>
 </section>
 <section class="call-to-action">

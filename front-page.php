c66464392f5f5f1d9be382aaf43738ab48a13939
Nick Eldred <knewedge@gmail.com>
2019-09-12 23:07:56 -0500

    added contact section

--- front-page.php
+++ front-page.php
@@ -93,14 +93,6 @@
 				</div>
 			</div>
 		</div>
-		<div class="row featured-copy">
-			<div class="col-md-4">
-				<img class="image-circle" src="/simantel/wp-content/uploads/2019/09/banner1-square.jpg" alt="kids loooking throuh fence" />
-			</div>
-			<div class="col-md-8">
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			</div>
-		</div>
 	</div>
 </section>
 <?php do_action( '__after_body' ); ?>

7c50e2da07c058c0b81f18af239c80f0daa5b225
Nick Eldred <knewedge@gmail.com>
2019-09-12 23:28:11 -0500

    renamed contact section to footer and broke out into new file

---
+++ footer.php
@@ -0,0 +1,40 @@
+<section class="custom-footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<h4 class="contact-head">Founding Members</h4>
+				<div class="row">
+					<div class="col-md-6">
+						<strong>HEADSHOT HERE</strong>
+					</div>
+					<div class="col-md-6">
+						<strong>HEADSHOT HERE</strong>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-8">
+				<div class="row">
+					<div class="col-md-4">
+						<strong>321 SW Water St</strong><br />
+						<strong>Peoria, IL 61602</strong>
+					</div>
+					<div class="col-md-4">
+						<strong>phone:</strong><br />
+						309.674.7747
+					</div>
+					<div class="col-md-4">
+						<strong>email:</strong><br />
+						info@simantel.com
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<i class="fab fa-facebook-square"></i>
+						<i class="fab fa-twitter"></i>
+						<i class="fab fa-instagram"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
--- front-page.php
+++ front-page.php
@@ -55,46 +55,7 @@
 		</div>
 	</div>
 </section>
-<section class="contact">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-4">
-				<h4 class="contact-head">Founding Members</h4>
-				<div class="row">
-					<div class="col-md-6">
-						<strong>HEADSHOT HERE</strong>
-					</div>
-					<div class="col-md-6">
-						<strong>HEADSHOT HERE</strong>
-					</div>
-				</div>
-			</div>
-			<div class="col-md-8">
-				<div class="row">
-					<div class="col-md-4">
-						<strong>321 SW Water St</strong><br />
-						<strong>Peoria, IL 61602</strong>
-					</div>
-					<div class="col-md-4">
-						<strong>phone:</strong><br />
-						309.674.7747
-					</div>
-					<div class="col-md-4">
-						<strong>email:</strong><br />
-						info@simantel.com
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-12">
-						<i class="fab fa-facebook-square"></i>
-						<i class="fab fa-twitter"></i>
-						<i class="fab fa-instagram"></i>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
+<?php include "footer.php"; ?>
 <?php do_action( '__after_body' ); ?>
 <?php get_footer(); ?>
 </html>
